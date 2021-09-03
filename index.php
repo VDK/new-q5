d5d2891b878d569a34510ab92f174f8505bccf50
Vera de Kok <veradekok@gmail.com>
2021-09-03 19:00:15 +0200

    fix html

--- index.php
+++ index.php
@@ -181,54 +181,56 @@ function appendProp($qid = null, $prop = null, $ref = null){
         <div>
         <div class="flex">
         <div style="width: 100%"><label for="fullname">name</label>
-        <input type="text" id="fullname"  name='fullname' ></div>
+        <input type="text" id="fullname"  name='fullname'/ ></div>
 
         <div><label for="age">age</label>
-        <input type="text" id="age"  name='age' style="width:100px;"></div>
+        <input type="text" id="age"  name='age' style="width:100px;"/></div>
       </div>
        <label for="dob">date of birth</label>
-        <input type="text" id="dob"  name='dob'  >
+        <input type="text" id="dob"  name='dob' />
         <label for="dod">date of death</label>
-        <input type="text" id="dod"  name='dod' >
+        <input type="text" id="dod"  name='dod'/ >
         <label for="description">short description</label>
-        <input type="text" id="description" name='description' >
+        <input type="text" id="description" name='description' />
         <label for="qs">custom QuickStatement</label>
-        <input type="text" id="qs" name='qs' >
+        <input type="text" id="qs" name='qs' />
         </div>
         <div id='possible_match' >
-          <input type="hidden" name="person_QID" id="person_QID">
+          <input type="hidden" name="person_QID" id="person_QID"/>
           <ul id='responses' ></ul>
         </div>
 
         Reference:
           <label for="ref_url">URL</label>
-          <input type="text" id="ref_url" placeholder="https://" name='ref_url' >
+          <input type="text" id="ref_url" placeholder="https://" name='ref_url' />
 
           <div id="ref_params" >
             <label for="ref_title">title</label>
             <div class="flex">
-              <input type="text" id="ref_lang" placeholder="lang" name="ref_lang" style="width:50px" maxlength="3" value="en">
-              <input type="text" id="ref_title" name="ref_title" >
+              <input type="text" id="ref_lang" placeholder="lang" name="ref_lang" style="width:50px" maxlength="3" value="en" />
+              <input type="text" id="ref_title" name="ref_title" />
             </div>
             <label for="ref_pubdate">publcation date</label>
-            <input type="date" id="ref_pubdate"  name="ref_pubdate" >
+            <input type="date" id="ref_pubdate"  name="ref_pubdate" />
             <label for="ref_authors">author</label>
-            <input type="text" id="ref_authors"  name="ref_authors" >
+            <input type="text" id="ref_authors"  name="ref_authors" />
           </div>
           <span style="clear:both;"/>
-        <input type="submit" class='button' value="go" id="submit">
-    </div>  
+        <input type="submit" class='button' value="go" id="submit" />
     </form>
-
+    </div>  
 <?php
 if ($qs ){?>
-   <div class="form-wrapper">
+  <div class="row">
+   <form class="form-wrapper">
 <textarea id='quickstatement' style="height: 150px;" >
 <?php echo($qs);  ?>
 </textarea>
 <a onclick="sendQS()" style="cursor: pointer;">Import QuickStatement</a>
-  </div>
+  </form>
+    </div>  
 <?php } ?>
+    </div>  
 
  <footer id="footer"><a href="https://github.com/VDK/New-Q5" target="_blank">Code on GitHub</a> <br/>available under MIT license</footer>
 </div>
