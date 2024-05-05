a8bf48b951d8977e10b2e3640621e53e093a3e7b
VDK <veradekok@gmail.com>
2024-05-05 11:59:08 +0300

    fixed styling of "described at URL", make label for checkbox clickable and a hyperlink out of the "described at URL" part of the label

--- index.php
+++ index.php
@@ -89,8 +89,9 @@ include_once 'controller.php';
             <input type="date" id="ref_pubdate"  name="ref_pubdate" />
             <label for="ref_authors">author</label>
             <input type="text" id="ref_authors"  name="ref_authors" />
-            <input id="described" type="checkbox" name="described_by_source" id="described_by_source" <?php echo $described_by_source; ?> />
-            <label id="described_label" for="described_by_source">Include reference as "described by url (P973)" statement</label>
+            <input id="described_by_source" type="checkbox" name="described_by_source" <?php echo $described_by_source; ?> />
+            <label id="described_label" for="described_by_source">Include reference as "<a href="https://www.wikidata.org/wiki/Property:P973" target="_blank">described by url (P973)</a>" statement</label>
+
           </div>
           <span style="clear:both;"/>
         <input type="submit" class='button' value="go" id="submit" />
